bedfcc83eb1cc500d0a29f3ed9b03364876f6bed
Christophe Lablancherie <clablancherie@cegep-ste-foy.qc.ca>
2020-01-15 09:47:11 -0500

    Modification du git ignore

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: clablancherie
+ * Date: 15/01/2020
+ * Time: 09:46
+ */
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class DefaultController extends AbstractController
+{
+
+}
